5f688aa0217c189a910128f4540b77b7595526bd
Moussa Fofana <mf008355@gmail.com>
2026-07-10 12:42:29 +0000

    fix: Dashboard button links to personal profile page /user/:id

--- plugins/claim.js
+++ plugins/claim.js
@@ -392,7 +392,7 @@ module.exports = {
                 const row = new ActionRowBuilder()
                     .addComponents(
                         new ButtonBuilder()
-                            .setURL('https://bamako-steel-dev.xyz/dashboard')
+                            .setURL(`https://bamako-steel-dev.xyz/user/${interaction.user.id}`)
                             .setLabel(t.viewDashboard)
                             .setStyle(ButtonStyle.Link)
                             .setEmoji('📊')
@@ -536,7 +536,7 @@ module.exports = {
             const actionRow = new ActionRowBuilder()
                 .addComponents(
                     new ButtonBuilder()
-                        .setURL('https://bamako-steel-dev.xyz/dashboard')
+                        .setURL(`https://bamako-steel-dev.xyz/user/${interaction.user.id}`)
                         .setLabel(t.viewDashboard)
                         .setStyle(ButtonStyle.Link)
                         .setEmoji('📊'),
@@ -647,7 +647,7 @@ module.exports = {
                 const row = new ActionRowBuilder()
                     .addComponents(
                         new ButtonBuilder()
-                            .setURL('https://bamako-steel-dev.xyz/dashboard')
+                            .setURL(`https://bamako-steel-dev.xyz/user/${interaction.user.id}`)
                             .setLabel(t.viewDashboard)
                             .setStyle(ButtonStyle.Link)
                             .setEmoji('📊')
@@ -787,7 +787,7 @@ module.exports = {
             const actionRow = new ActionRowBuilder()
                 .addComponents(
                     new ButtonBuilder()
-                        .setURL('https://bamako-steel-dev.xyz/dashboard')
+                        .setURL(`https://bamako-steel-dev.xyz/user/${interaction.user.id}`)
                         .setLabel(t.viewDashboard)
                         .setStyle(ButtonStyle.Link)
                         .setEmoji('📊'),
